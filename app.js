4719328189530918143208295920e36783393d4b
Roshith <107235573+roshith844@users.noreply.github.com>
2022-11-15 13:12:41 +0000

    Add : Server and Home page Route
    - Use 'Ejs' view engine
    - Use 'ejs-express-layouts' npm module for setting layout

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const userRouter = require('./routes/userRoutes')
+
+// Middlewares
+app.use("/", userRouter);
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set("layout", "./layouts/layout.ejs");
+
+app.listen(8000, () => {
+    console.log("Server Started")
+})
---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const expressLayouts = require('express-ejs-layouts');
+
+router.use(expressLayouts);
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+module.exports = router
