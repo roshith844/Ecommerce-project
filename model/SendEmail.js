683a61ee08eba159dd963d78063fc9468a868bec
Roshith <roshith844@gmail.com>
2022-11-22 13:14:23 +0000

    reformat

--- model/SendEmail.js
+++ model/SendEmail.js
@@ -1,32 +1,32 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
-function sendMail(randomCode, UserEmail){
+function sendMail(randomCode, UserEmail) {
 
-// Sends email
-const mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.FROM_EMAIL,
-        pass: process.env.APP_PASSWORD
+    // Sends email
+    const mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.FROM_EMAIL,
+            pass: process.env.APP_PASSWORD
+        }
+    })
+
+    // Email content
+    const details = {
+        from: 'roshith844@gmail.com',
+        to: UserEmail,
+        subject: 'Your Otp for Cake shop',
+        text: 'Your OTP for cake shop is ' + randomCode
     }
-})
 
-// Email content
-const details = {
-    from: 'roshith844@gmail.com',
-    to: UserEmail,
-    subject: 'Your Otp for Cake shop',
-    text: 'Your OTP for cake shop is ' + randomCode
-}
-
-mailTransporter.sendMail(details, (err) => {
-    if (err) {
-        console.log('mail error :', err)
-    } else {
-        console.log('email send')
-    }
-})
+    mailTransporter.sendMail(details, (err) => {
+        if (err) {
+            console.log('mail error :', err)
+        } else {
+            console.log('email send')
+        }
+    })
 }
 
 module.exports = sendMail
