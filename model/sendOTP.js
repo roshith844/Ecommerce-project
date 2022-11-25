6dcbb065f927c6576f8f45cae935c0a94b57d51b
Roshith <roshith844@gmail.com>
2022-11-25 05:43:18 +0000

    reformat: remove unnecessary comments.

--- model/sendOTP.js
+++ model/sendOTP.js
@@ -14,31 +14,6 @@ function sendOTP(randomCode, userNumber) {
     })
     .then((message) => console.log(`Message SID ${message.sid}`))
     .catch((error) => console.error(error));
-
-  // // Sends email
-  // const mailTransporter = nodemailer.createTransport({
-  //     service: 'gmail',
-  //     auth: {
-  //         user: process.env.FROM_EMAIL,
-  //         pass: process.env.APP_PASSWORD
-  //     }
-  // })
-
-  // // Email content
-  // const details = {
-  //     from: 'roshith844@gmail.com',
-  //     to: UserEmail,
-  //     subject: 'Your Otp for Cake shop',
-  //     text: 'Your OTP for cake shop is ' + randomCode
-  // }
-
-  // mailTransporter.sendMail(details, (err) => {
-  //     if (err) {
-  //         console.log('mail error :', err)
-  //     } else {
-  //         console.log('email send')
-  //     }
-  // })
 }
 
 module.exports = sendOTP
