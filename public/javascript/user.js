4a7f25f26b9af32976def75f7669200a3627be4a
Roshith P <roshith844@gmail.com>
2024-11-16 16:18:24 +0530

    fix: cart alert should only show once

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,20 +1,31 @@
 function addToCart(productId) {
-     $.ajax({
-       url: "/add-to-cart/" + productId,
-       method: "get",
-       success: (response) => {
-         if (response.status) {
-           let count = $("#cartCount").html();
-           count = parseInt(count) + 1;
-           $("#cartCount").html(count);
-           // alert  messege
-           $("#alerts").append(
-             '<div  class="alert alert-success alert-dismissible fade show" role="alert"> Item Added to Cart! <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-           );
-         }
-       },
-     });
-   }
+  $.ajax({
+    url: "/add-to-cart/" + productId,
+    method: "get",
+    success: (response) => {
+      if (response.status) {
+        let count = $("#cartCount").html();
+        count = parseInt(count) + 1;
+        $("#cartCount").html(count);
+
+        // Remove any existing alert before appending a new one
+        $(".alert").remove();
+
+        let alert = $(
+          '<div class="alert alert-success alert-dismissible fade show" role="alert"> Item Added to Cart! <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        );
+        $("#alerts").append(alert);
+        setTimeout(() => {
+          alert.fadeOut(500, function () {
+            $(this).remove(); // Remove the alert element after fading out
+          });
+        }, 2000);
+        
+      }
+    },
+  });
+}
+
 
    function addToWishlist(productId) {
      $.ajax({
